341886dda537eb24d3fbc2ec7158fe8f604ec169
Prosper Atukwatse <pa5@williams.edu>
2019-06-11 15:43:05 +0300

    Add flask app

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask
+
+from buzz import generator
+
+app = Flask(__name__)
+
+@app.route('/')
+def generate_buzz():
+	page = """
+			<html>
+				<body>
+					<h1>{0}</h1>
+				</body>
+			</html>
+			""".format(generator.generate_buzz())
+	return page
+
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
